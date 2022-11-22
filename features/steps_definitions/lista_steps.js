187d0b61b40c8b32999835b8a332a8cdfa4fe002
Gustavo Bastian <bastian.gustavo@gmail.com>
2022-11-22 13:36:36 -0300

    cambio de archivo lista, mejor test

--- features/steps_definitions/lista_steps.js
+++ features/steps_definitions/lista_steps.js
@@ -2,84 +2,79 @@ const {Giver, When, Then, Given} = require('@cucumber/cucumber')
 const expect= require('chai').expect;
 const assert= require('chai').assert;
 const Lista = require("../../src/lista");
-
 var lista;
 
-Given('una lista vacía',()=>{
+Given('una lista vacía',()=>
+{
     lista=new Lista();    
 });
 
 
-When('se agrega la pareja {}',(word)=>{
-       
-    
-    let data=word.split(",")
-    let data2=data[0].split("\"")
-    let data3=data[1].split("\"")
-    let key=(data2[1])
-    let value=(data3[1])
-    
-    
-    lista.add(key,value);  
+When('se agrega la pareja {}',(word)=>
+{   
+  let data=word.split(",")
+  let data2=data[0].split("\"")
+  let data3=data[1].split("\"")
+  let key=(data2[1])
+  let value=(data3[1])
+  lista.add(key,value);  
 });
 
-Then('cuando busco el valor asociado a la clave {} el resultado es {}',(key,response)=>{    
-    let data=key.split("\"")
-    assert.equal(lista.find(data[1]),response);
+Then('cuando busco el valor asociado a la clave {} el resultado es {}',(key,response)=>
+{    
+  let data=key.split("\"")
+  assert.equal(lista.find(data[1]),response);
 });
 
+Then('la lista tiene {int} elemento\\(s) almacenado\\(s)', (value)=>
+{
+  assert.equal(lista.count(),value);
+});
 
-Then('la lista tiene {int} elemento\\(s) almacenado\\(s)', (value)=>{
-      assert.equal(lista.count(),value);
-    });
-
-
-Then('la lista retorna \\{\\{{string},{string}},\\{{string},{string}},\\{{string},{string}}}', async (string, string2, string3, string4, string5, string6) =>{
+Then('la lista retorna \\{\\{{string},{string}},\\{{string},{string}},\\{{string},{string}}}', async (string, string2, string3, string4, string5, string6) =>
+{
         
-        let responseArray=[];
-        await responseArray.push({key:string,value:string2});
-        await responseArray.push({key:string3,value:string4});
-        await responseArray.push({key:string5,value:string6});
-
-        let responseArrayLocal=await lista.list();        
-
-        for( let i=0; i<responseArray.length; i++)
-        {
-            assert.equal(responseArrayLocal[i].value,responseArray[i].value);
-            assert.equal(responseArrayLocal[i].key,responseArray[i].key);
-        }        
-
-        
-      });
+  let responseArray=[];
+  await responseArray.push({key:string,value:string2});
+  await responseArray.push({key:string3,value:string4});
+  await responseArray.push({key:string5,value:string6});
+  let responseArrayLocal=await lista.list();        
+  for( let i=0; i<responseArray.length; i++)
+  {
+    assert.equal(responseArrayLocal[i].value,responseArray[i].value);
+    assert.equal(responseArrayLocal[i].key,responseArray[i].key);
+  }     
+});
 
 // Busqueda fallida
-Then('cuando busco el valor asociado a la clave \\{{string}} no cargado retorna falla', (string)=>{        // Write code here that turns the phrase above into concrete actions
-        assert.equal(lista.find(string),null);
-      });
+Then('cuando busco el valor asociado a la clave \\{{string}} no cargado retorna falla', (string)=>
+{
+   assert.equal(lista.find(string),null);
+});
 
 // Eliminación de elementos
-When('se elimina la clave \\{{string}}', async function (string) {
-        let response= await lista.delete(string);
-        assert.equal(response,true);            
-      });
+When('se elimina la clave \\{{string}}', async function (string) 
+{
+  let response= await lista.delete(string);
+  assert.equal(response,true);            
+});
 
 // Eliminación de elementos
-When('se elimina la clave \\{{string}} retorna error',async function (string) {
-    let response= await lista.delete(string);
-    assert.equal(response,false);   
-  });      
-
+When('se elimina la clave \\{{string}} retorna error',async function (string) 
+{
+  let response= await lista.delete(string);
+  assert.equal(response,false);   
+});      
 
 //alteración de valores
-When('se modifica la clave \\{{string},{string}}', async function (string, string2) {
-    
-    let response= await lista.update(string,string2);
-    assert.equal(response,true);  
-    
-  });  
+When('se modifica la clave \\{{string},{string}}', async function (string, string2) 
+{
+  let response= await lista.update(string,string2);
+  assert.equal(response,true);  
+});  
 
-When('se modifica la clave \\{{string},{string}} da error', async function (string, string2) {
-    // Write code here that turns the phrase above into concrete actions
-    let response= await lista.update(string,string2);
-    assert.equal(response,false);  
-  });  
+When('se modifica la clave \\{{string},{string}} da error', async function (string, string2) 
+{
+  let response= await lista.update(string,string2);
+  assert.equal(response,false);  
+});  
--- src/lista.js
+++ src/lista.js
@@ -1,137 +1,136 @@
-module.exports = class Lista{
-    constructor(){        
-        this.localList=[]
+module.exports = class Lista
+{
+    constructor()
+    {          
+        this.Elementos=[];
+    };
+
+    count()
+    {        
+        return  this.Elementos.length;
     }
 
-    //adding elements to the list
-    async add(keyP,valueP){            
-        let element={key:keyP,value:valueP}
-        let result=false;
-        let auxiliarArray=[];
-
-        if(((keyP==null) || (valueP==null))||(typeof(keyP)!='string')||(typeof(valueP)!='string')||(valueP=="")||(keyP==""))
+    add(clave,valor)
+    {
+        let auxiliar=false;
+        let arregloAuxiliar=[];
+        let elementoLocal={'clave':clave,'valor':valor};
+        let lenElements=this.Elementos.length;
+        if(((clave==null) || (valor==null))||(typeof(clave)!='string'))
         {
             return false;
         }       
-    
-        let elementLocalS=await this.find(keyP)
-        
-        
-        if((elementLocalS==null))
+        let busqueda= this.find(clave);        
+        if (busqueda==null)
         {
-            
-            result=true;
-
+            auxiliar=true;            
         }
-        else 
+        if (auxiliar==true)
+        {
+            //caso especial, no hay elementos
+            if (this.Elementos.length==0)
+            {
+                this.Elementos.push(elementoLocal); 
+                return true;     
+            }
+            //caso especial, el nuevo valor es mayor que el mayor de la lista
+            else if ((this.Elementos[lenElements-1].clave)<(elementoLocal.clave))
+            {               
+                this.Elementos.push(elementoLocal); 
+                return true;     
+            }
+            else
+            {                
+                let index=0;                                
+                while ((this.Elementos[index].clave)<(elementoLocal.clave))
+                {                   
+                    arregloAuxiliar.push(this.Elementos[index]);
+                    index++;
+                    if (index==this.Elementos.length-1)
+                    {
+                        break;
+                    }
+                }
+                arregloAuxiliar.push(elementoLocal);
+                while (index<this.Elementos.length)
+                {
+                    arregloAuxiliar.push(this.Elementos[index]);                
+                    index++;
+                }                
+                this.Elementos=arregloAuxiliar;
+                return true;
+            }
+        }
+        else
         {
             return false;
         }
+    }
 
-        //special case first element
-        if(this.count()==0){
-            this.localList.push(element);
-            return true;
-        }
-        //special case value higher than maximun
-        else if(this.localList[this.count()-1].value<element.value)
+    find(clave)
+    {
+        let value=null; 
+        let indice=this.indexOf(clave);        
+        if (indice!=null)
         {
-
-            this.localList.push(element);
-            return true;
+            value=this.Elementos[indice].valor;
         }
-        else{
-            let index=0;                
-                
-            while ((parseInt(this.localList[index].value.toString())<parseInt(element.value.toString()))&&(index<this.localList.length-1)){                   
-                auxiliarArray.push(this.localList[index])              
-                index++;
-            }
-            
-            auxiliarArray.push(element)
-            
-            while (index<this.localList.length){
-                auxiliarArray.push(this.localList[index]);                
-                index++;
-            }
-            
-
-            this.localList=auxiliarArray;
-
-        }
-
-
-        return result;
-    }
-    //size of the list
-    count(){
-        return this.localList.length;
-    }
-
-    find(keyP){
-        let keyLocal={key:keyP}        
-        let localValue=null
-        this.localList.forEach(element => {            
-            
-            if((element.key)==keyLocal.key)
-                {
-                    
-                    localValue="\""+element.value+"\"";
-                }
-        });
-        return localValue;
-    }
-    //return of the ordered list
-    list(){
-        return this.localList;
-    }
-    //looking for the index
-    getIndexOf(key){
-
-        let index=null;
-        for (let i=0; i<this.localList.length; i++)
+        else 
         {
-            if(this.localList[i].key==key)
-            {
-                index=i;
-            }
+            value=null;
         }
-        return index;
+        return value;
     }
-    //removing an element from the list
-    async delete(key)
-    {        
-        let retorno= await this.find(key);
-        if(retorno==null)
+
+    delete(clave)
+    {                
+        let resultado= this.indexOf(clave);                        
+        if (resultado==null)
         {            
             return false;
         }
         else
-        {
-            let index=this.getIndexOf(key);            
-            this.localList.splice(index,1);
+        {            
+            this.Elementos.splice(resultado,1)
             return true;
         }
-        
     }
-    //updating an element in the list
-    async update(key,value)
-    {
-        if(((key==null) || (value==null))||(typeof(key)!='string')||(typeof(value)!='string')||(value=="")||(key==""))
-        {
-            return false;
-        }       
-        let retorno= await this.find(key);
 
-        if(retorno==null)
-        {           
-            return false;
+    indexOf(clave)
+    {
+        let value=null;        
+        for (let i = 0; i < this.Elementos.length; i++) 
+        {
+            if (clave == this.Elementos[i].clave) 
+            {
+                return value = i;
+            }
+        };        
+        return value;
+    }
+
+    update(clave,valor)
+    {
+        let result= this.find(clave);
+        if (result!=null)
+        {            
+            let indice=this.indexOf(clave);
+            this.Elementos[indice].valor=valor;
+            return true;
         }
         else
-        {
-            let index=this.getIndexOf(key);            
-            this.localList[index].value=value;
-            return true;
+        {            
+            return false;
         }
     }
-}
+
+    getLista()
+    {
+        let output=[]
+        this.Elementos.forEach(element => 
+        {
+            output.push({'clave':element.clave})
+        });        
+        return   output;        
+    }
+};
